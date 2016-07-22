e6e760f2530d1514031166a34c104ae6ca19515f
lili <kk@163.com>
2016-07-22 11:03:06 +0800

    Activity

---
+++ app/src/main/java/com/a/activitynew/ActivitySecond.java
@@ -0,0 +1,12 @@
+package com.a.activitynew;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+//这是第二个Activity,是新建了一个类，继承于AppCompatActivity，就变成了一个Activity
+public class ActivitySecond extends AppCompatActivity {
+
+}
